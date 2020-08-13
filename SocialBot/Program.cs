04fe5134ad1544fe52e80a4b742328129fd81e78
try1975 <try1975@mail.ru>
2020-08-13 18:44:29 +0300

    all done

--- SocialBot/Program.cs
+++ SocialBot/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HtmlAgilityPack;
+using System;
 
 namespace SocialBot
 {
@@ -10,6 +7,22 @@ namespace SocialBot
     {
         static void Main(string[] args)
         {
+            var url = @"https://www.kellysubaru.com/used-inventory/index.htm";
+            
+            var web = new HtmlWeb();
+            var htmlDoc = web.Load(url);
+
+            var node = htmlDoc.QuerySelector("li.green-vehicle");
+            var img = node.QuerySelector("img");
+            string src = img.GetAttributeValue("src", string.Empty);
+            var price = node.QuerySelector("span.value");
+            var vin = node.QuerySelector("dl.vin dd");
+            
+            Console.WriteLine($"Img url: {src}");
+            Console.WriteLine($"Price : {price.InnerText}");
+            Console.WriteLine($"VIN : {vin.InnerText}");
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
         }
     }
 }
